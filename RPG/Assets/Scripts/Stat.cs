a977e3b82405e47c52f1e187d470c9a0daa3a96d
spartann123 <robertojr_hofmann@hotmail.com>
2018-02-02 13:30:29 -0200

    update

---
+++ RPG/Assets/Scripts/Stat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour
+{
+
+	private Image content;
+
+	public float currentFill;
+
+
+	public float MyMaxValue { get; set; }
+
+	public float MyCurrentValue {
+		get {
+			return currentValue;
+		}
+		set {
+
+			if (value > MyMaxValue) {
+				currentValue = MyMaxValue;
+			} else if (value < 0) {
+				currentValue = 0;
+			} else {
+				currentValue = value;
+			}
+		}
+	}
+
+	public float currentValue;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		MyMaxValue = 100;
+		content = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
